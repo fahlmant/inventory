a95c7bcb65b27e537c438d8d35276a74b18de32a
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-06-30 15:56:25 -0700

    remove commands from notes since most are in script already and its cluttered, add comments for each section of inventory shell scriptw

--- inventory.sh
+++ inventory.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-#Get Hostname
+#1) Get Hostname
 HOSTNAME=$(hostname -f)
 echo "hostname: $HOSTNAME"
-#Get Serial number for system
+
+#2) Get Serial number for system
 sudo dmidecode --type system | egrep -i 'System Information' -A 10 | grep Serial | sed -e 's/^[[:space:]]*//g'
-#Get each device and mac adress
+
+#3) IP Address
+
+#4) Get each device and mac adress
 for f in /sys/class/net/*; do
   echo  "$f"
   cat $f/address
 done
+
+#5) IPMI Mac Addr
+#6) IPMI User
+#7) IPMI Password
+
+#8) Disk Info
+
+#9) CPU Info
+lscpu
+
+#10) Ram Info
+
 printf "\n"
