636b5d961f0987f424a0b9c74f855e831adf9c24
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-07-05 11:24:37 -0700

    Remove old commands and comments

--- inventory.sh
+++ inventory.sh
@@ -13,8 +13,6 @@ cd /tmp/$MAYDAY_DIR_DATE
 
 #1) Get Hostname via Mayday
 cat hostname
-#HOSTNAME=$(hostname -f)
-#echo "#^hostname: $HOSTNAME"
 
 #2) Get Serial number for system
 sudo dmidecode --type system | egrep -i 'System Information' -A 10 | grep Serial | sed -e 's/^[[:space:]]*//g' | awk '{print "#^" $0}'
@@ -26,12 +24,6 @@ cat ip_addr_show | awk '/eth0/ {print $4}'
 #4) Mac Address via Mayday for eth0
 cat ip_link_show | awk '/eth0/ {print $17}'
 
-#printf "#^Mac Addresses:\n"
-#for f in /sys/class/net/*; do
-#  printf "$f"
-#  printf "%s"$(cat $f/address)
-#done
-
 #5) IPMI Mac Addr
 
 
@@ -42,14 +34,11 @@ cat ip_link_show | awk '/eth0/ {print $17}'
 
 
 #8) Disk Info
-#printf "#^Disk Info:\n"
 df -h
 
 #9) CPU Info
-#printf "#^CPU Info:\n"
 lscpu | awk '/Arch|mode|Order|CPU(s)|Model name|CPU MHz/'
 
 #10) Ram Info via Mayday
-#printf "#^RAM Info:\n"
 cat free
 printf "\n"
