4499ef5620a2d0fbe189fe6fffd3b89ec91a87c6
Taylor Fahlman <fahlmantaylor@gmail.com>
2017-07-06 13:10:04 -0700

    Reciever now writes output to files based on hostnames, inventroy a little cleaned up

--- inventory.sh
+++ inventory.sh
@@ -2,28 +2,31 @@
 
 #1) Get Hostname
 HOSTNAME=$(hostname -f)
-echo "hostname: $HOSTNAME"
+printf "#^Hostname: $HOSTNAME"
 
 #2) Get Serial number for system
-sudo dmidecode --type system | egrep -i 'System Information' -A 10 | grep Serial | sed -e 's/^[[:space:]]*//g'
+sudo dmidecode --type system | egrep -i 'System Information' -A 10 | grep Serial | sed -e 's/^[[:space:]]*//g' | awk '{print "#^" $0}'
 
 #3) IP Address
+printf "#^IP Adresses:\n"
+ip -o addr show  | awk '{print $2 " " $3 " " $4}'
 
-#4) Get each device and mac adress
-printf "Mac Addresses:\n"
-for f in /sys/class/net/*; do
-  printf "\t$f"
-  printf "\t%s"$(cat $f/address)
-done
+#4) Get Mac Address
+printf "#^MAC Adresses:\n"
+ip -o link show | awk '/eth0/ {print $2 " " $17}'
 
 #8) Disk Info
+printf "#^Disk Info:\n"
 df -h
 
 #9) CPU Info
-printf "CPU Info:\n"
+printf "#^CPU Info:\n"
 lscpu | awk '/Arch|mode|Order|CPU(s)|Model name|CPU MHz/'
 
 #10) Ram Info
+printf "#^Memory Info:\n"
 free -m
+free -mh
 
+printf "#^END"
 printf "\n"
--- reciever.py
+++ reciever.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 #Importing things
 import socket
 import sys
@@ -11,11 +10,17 @@ def signal_handler(signal, frame):
         print('Cleanly exiting')
         sys.exit(0)
 
+def get_hostname(buf):
+    result = buf.split("#^Hostname: ", 1)[1]
+    print ("RESULT " + result)
+    return result
+
 #Set up socket bindings and signal listener
 port = 5000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(("", port))
 signal.signal(signal.SIGINT, signal_handler)
+info_buffer = " "
 
 #Start listening on port
 print("waiting on port:", port)
@@ -24,3 +29,15 @@ while 1:
     data, addr = s.recvfrom(2048)
     #Stripe \n from strings and decode bytes"
     print(data.rstrip().decode())
+    info_buffer += data.rstrip().decode()
+    if "#^Hostname" in data.rstrip().decode():
+        buf = data.rstrip().decode()
+        hostname = get_hostname(buf)
+    if "#^END" in data.rstrip().decode():
+        print ("HERE")
+        info_file = open('servers/' + hostname, "w+")
+        info_file.write(info_buffer)
+        buf = " "
+        hostname = " " 
+        info_file.close()
+        info_buffer = " "
